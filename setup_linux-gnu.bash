6dd0ff1218a8a6c5211f809c2a9b09f071438e79
Christian Lengert <christian.lengert@inovex.de>
2022-11-14 22:30:58 +0100

    Refactor setup_linux-gnu.bash to use merge_config_file

--- setup_linux-gnu.bash
+++ setup_linux-gnu.bash
@@ -2,8 +2,8 @@
 echo "Setting up ${OSTYPE}"
 source helpers.bash
 
-copy_config_file .zshrc.linux-gnu ~/.zshrc
-copy_config_file .vimrc ~/
-copy_config_file kitty.conf.linux-gnu ~/.config/kitty/kitty.conf
+merge_config_file .zshrc.common .zshrc.linux-gnu ~/.zshrc
+merge_config_file kitty.conf.common kitty.conf.linux-gnu ~/.config/kitty/kitty.conf
 copy_config_file ${KITTY_THEME} ~/.config/kitty/current-theme.conf
+copy_config_file .vimrc ~/
 copy_config_file .tmux.conf ~/

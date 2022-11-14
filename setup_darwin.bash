9ebc5ec4976f7ab51bd873885c68162656f18e4a
Christian Lengert <christian.lengert@inovex.de>
2022-11-14 22:17:22 +0100

    Extract common configuration for kitty

--- setup_darwin.bash
+++ setup_darwin.bash
@@ -5,7 +5,9 @@ source helpers.bash
 cp files/.zshrc.common ~/.zshrc
 cat files/.zshrc.darwin >> ~/.zshrc
 
-copy_config_file .vimrc ~/
-copy_config_file kitty.conf.darwin ~/.config/kitty/kitty.conf
+cp files/kitty.conf.common ~/.config/kitty/kitty.conf
+cat files/kitty.conf.darwin >> ~/.config/kitty/kitty.conf
 copy_config_file ${KITTY_THEME} ~/.config/kitty/current-theme.conf
+
+copy_config_file .vimrc ~/
 copy_config_file .tmux.conf ~/

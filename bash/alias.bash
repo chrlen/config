bf23d3e7972d8b6dd7ec7617863751d03992a2c7
Christian Lengert <christian.lengert@inovex.de>
2022-11-23 10:51:50 +0100

    add aliases for git

--- bash/alias.bash
+++ bash/alias.bash
@@ -9,8 +9,14 @@ alias v=nvim
 alias wfo=windscribe firewall on
 alias wfa=windscribe firewall auto
 alias scrape=wget -H -r --level=5 --restrict-file-names=windows --convert-links -e robots=off
-alias gac=git add -A && git commit -m
 alias restart_plasma=killall plasmashell && kstart5 plasmashell && killall krunner
 alias inbuilt_keyboard=setxkbmap -layout de-latin1
 alias thinkpad_keyboard=setxkbmap -layout us -variant de_se_fi
 alias fnd=find ./ -name
+
+
+# Git
+alias gac="git add -A && git commit -m"
+alias gf="git fetch"
+alias gp="git pull"
+alias gs="git status"

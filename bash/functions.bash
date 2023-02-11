6bb51bd9a3705cf3ae3668eccb0cbeaa317706fa
Christian <christian.lengert@tuta.io>
2023-02-11 17:06:06 +0100

    Improve zsh startup speed

--- bash/functions.bash
+++ bash/functions.bash
@@ -1,5 +1,25 @@
 #! /bin/bash
 
 toJpg(){
-    find ${1} -name "*.CR2" | parallel -I% --max-args 1 convert % -sampling-factor 4:2:0 -strip  -quality 85 -interlace JPEG -colorspace sRGB %.jpg
+	find ${1} -name "*.CR2" | parallel -I% --max-args 1 convert % -sampling-factor 4:2:0 -strip  -quality 85 -interlace JPEG -colorspace sRGB %.jpg
+}
+
+
+setupMamba(){
+	__conda_setup="$('/usr/local/Caskroom/mambaforge/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+	if [ $? -eq 0 ]; then
+		eval "$__conda_setup"
+	else
+		if [ -f "/usr/local/Caskroom/mambaforge/base/etc/profile.d/conda.sh" ]; then
+			. "/usr/local/Caskroom/mambaforge/base/etc/profile.d/conda.sh"
+		else
+			export PATH="/usr/local/Caskroom/mambaforge/base/bin:$PATH"
+		fi
+	fi
+	unset __conda_setup
+}
+
+
+timeZshStartup(){
+	/usr/bin/time zsh -i -c exit	
 }
